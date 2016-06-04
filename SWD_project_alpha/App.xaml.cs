fe1d88982ba7e76416bfa4bb9a9e9b78e8802866
rezik007 <rezik007@gmail.com>
2016-06-04 20:52:58 +0200

    ok
    ok

---
+++ SWD_project_alpha/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SWD_project_alpha
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SWD_project_alpha/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SWD_project_alpha
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+                MessageBox.Show("HELLO WORLD");
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(((data2.SelectedDate)-(data1.SelectedDate)).Value.TotalDays.ToString());
+            int a = 100, b = 100;
+            int[,] tablica = new int[a, b];
+            tablica[0,0] = 1;
+            
+
+        }
+    }
+}
