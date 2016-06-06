778d6028c9538e97454bd202c5ac09205e53281b
rezik007 <rezik007@gmail.com>
2016-06-07 01:47:41 +0200

    bebechy
    wnetrze prawie gotowe, teraz warstwa prezentacji + fakty

--- SWD_project_alpha/MainWindow.xaml.cs
+++ SWD_project_alpha/MainWindow.xaml.cs
@@ -20,6 +20,14 @@ namespace SWD_project_alpha
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int smallDays = 0, mediumDays = 0, longDays = 0;
+        public int horse50 = 0, horse100 = 0, horse150 = 0, horse200 = 0;
+        public int seats4 = 0, seats5 = 0, seats7 = 0;
+        public int gearType = 0; //domyslnie manualna skrzynia
+        public string tableString;
+        public int[] tableBits;
+        public int[] factsTable = new int[10];
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +35,8 @@ namespace SWD_project_alpha
 
         private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
-                MessageBox.Show("HELLO WORLD");
+
+
         }
 
         private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -37,11 +46,72 @@ namespace SWD_project_alpha
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(((data2.SelectedDate)-(data1.SelectedDate)).Value.TotalDays.ToString());
-            int a = 100, b = 100;
-            int[,] tablica = new int[a, b];
-            tablica[0,0] = 1;
+            double numberOfDays = ((data2.SelectedDate) - (data1.SelectedDate)).Value.TotalDays;
+            if (numberOfDays<1)
+                MessageBox.Show("Wybierz poprawna liczbe dni");
+            else if ((numberOfDays > 0) && (numberOfDays < 4))
+                smallDays = 1;
+            else if ((numberOfDays > 3) && (numberOfDays < 7))
+                mediumDays = 1;
+            else if (numberOfDays > 6)
+                longDays = 1;
+
+           // MessageBox.Show(numberOfDays.ToString());
             
+            if (checkBox.IsChecked == true)
+                gearType = 1; // jak zaznaczone to automat
+            switch (comboBox.SelectedIndex) //wybor mocy silnika
+            {
+                case 0:
+                    horse50 = 1;
+                    break;
+
+                case 1:
+                    horse100 = 1;
+                    break;
+
+                case 2:
+                    horse150 = 1;
+                    break;
+
+                case 3:
+                    horse200 = 1;
+                    break;
+            }
+            switch (comboBox2.SelectedIndex) //wybor ilosci siedzen
+            {
+                case 0:
+                    seats4 = 1;
+                    break;
+
+                case 1:
+                    seats5 = 1;
+                    break;
+
+                case 2:
+                    seats7 = 1;
+                    break;
+            }
+            // this.tableStart();
+     
+        }
+        private void tableStart()
+        {
+            for (int i = 0; i < 2048; i++)
+            {
+                tableString = Convert.ToString(i, 2); //Convert to binary in a string
+                tableBits = tableString.PadLeft(11, '0') // Add 0's from left
+                             .Select(c => int.Parse(c.ToString())) // convert each char to int
+                             .ToArray(); // Convert IEnumerable from select to Array
+                                         //table_counter = table_counter << 1 ;
+                                         //sprawdzanie faktow
+                if (tableBits[0] == 1)//tutaj pisezemy caly pojedynczy fakt
+                    factsTable[0] = 1; // i jesli jest spelniony to wstawiamy jedynke (to samo dla Fy)
+                else if (tableBits[1] == 1)
+                    factsTable[1] = 0;
+
+            }
+            // MessageBox.Show(ta.ToString());
 
         }
     }
